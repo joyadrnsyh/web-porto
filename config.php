5579e54209da9c1bcc526560d9ac66ff7fc62dce
Joy Adriansyah <joy85093@gmail.com>
2025-12-13 17:50:34 +0700

    Prepare for Vercel deployment

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'web_porto');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_NAME', getenv('DB_NAME') ?: 'web_porto');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
 
 define('UPLOAD_DIR', __DIR__ . '/assets/uploads/');
 
